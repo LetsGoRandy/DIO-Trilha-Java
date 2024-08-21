18ae63dff113435b6a97c4787aa1071d9776bc14
Randy Gomes <raandy.gc@gmail.com>
2024-08-21 19:34:01 -0300

    Criação da ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //  TODO:  Conhecer e importar a classe Scanner;
+        //         Exibir as mensagens para o osuário;
+        //         Obter pelo scanner os valores digitados no terminal;
+        //         Exibir a mensagem conta criada;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Olá, seja bem vindo ao Banco DIO!");
+        System.out.println("Digite o numero da conta: ");
+        int numeroConta = input.nextInt();
+        System.out.println("Digite o numero da agência: ");
+        String numeroAgencia = input.next();
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = input.next();
+        System.out.println("Digite o saldo inicial: ");
+        Double saldoInicial = input.nextDouble();
+
+        System.out.println("Conta Criada com sucesso!");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco! Sua agência é: " + numeroAgencia + ", sua conta é: " + numeroConta + "., e seu saldo é de: R$ " + saldoInicial + " e já está disponível para ser sacado!" );
+                 
     }
 }
